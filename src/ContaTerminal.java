b15034182b2f4191b73b68a6ba13d641cddd89c2
Lucas Roveder <lucasroveder@gmail.com>
2023-10-02 17:48:04 -0300

    incrementacao dos metodos deposito, verificar conta e saque

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -45,6 +45,21 @@ public class ContaTerminal {
         this.saldo = saldo;
     }
 
+    public void fazDeposito(double n){
+        this.saldo = this.saldo + n;
+    }
+
+    public int fazSaque(double n){
+        if (n>this.saldo){
+            System.out.println("Saldo insuficiente");
+            return 0;
+        }
+        else{
+            this.saldo = this.saldo - n;
+            return 1;
+        }
+    }
+
     @Override
     public String toString() {
         return "Olá " + nome + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque";
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // criando variaves utilizadas em todo o codigo
@@ -7,7 +8,8 @@ public class Main {
         int posicaoContas = 0;
         Scanner sc = new Scanner(System.in);
         // faz um menu interativo com o usuario
-        while(menu1 == 1){
+        int menu2 = 0;
+        while (menu1 == 1) {
             int i = 0;
             System.out.println("===============================================================");
             System.out.println("Ola, cliente o que deseja fazer?");
@@ -16,11 +18,11 @@ public class Main {
             System.out.println("3. Sair");
             System.out.println("===============================================================");
             i = sc.nextInt();
-            if(i!=1 && i!=2 && i!=3){
+            if (i != 1 && i != 2 && i != 3) {
                 System.out.println("insira um numero valido");
                 System.out.println("retornando ao menu 1");
             }
-            if(i == 1){
+            if (i == 1) {
                 sc.nextLine();
                 System.out.println("Digite seu nome completo");
 
@@ -30,7 +32,6 @@ public class Main {
                 System.out.println("Digite o numero da sua agencia xxx-x (Exemplo 123-4)");
                 String agencia = sc.nextLine();
                 System.out.println("Sua agencia é: " + agencia);
-
 
                 System.out.println("Digite o numero desejado para sua conta (Exemplo: 1021)");
                 int numero = sc.nextInt();
@@ -43,7 +44,7 @@ public class Main {
                 contas[posicaoContas] = new ContaTerminal(numero, agencia, nome, saldo);
                 posicaoContas++;
             }
-            if (i == 2){
+            if (i == 2) {
                 sc.nextLine();
                 System.out.println("Digite sua agencia (Exemplo: 123-4)");
                 String agencia = sc.nextLine();
@@ -51,28 +52,56 @@ public class Main {
                 System.out.println("Digite o numero da sua conta no formato xxxx (Exemplo: 1021)");
                 int numero = sc.nextInt();
 
-                for (int verificaAgencias = 0; verificaAgencias<5; verificaAgencias++){
-                    if(
-                    agencia.equals(contas[verificaAgencias].getAgencia()) &&
-                    numero == contas[verificaAgencias].getNumero()){
-                        System.out.println("O saldo do cliente " + contas[verificaAgencias].getNome() + " é de: " + contas[verificaAgencias].getSaldo());
-                        break;
-                    }
-                    else if(verificaAgencias !=4){
-                        System.out.println("Verificando....");
-                    }
-                    else{
+                for (int verificaAgencias = 0; verificaAgencias < 5; verificaAgencias++) {
+                    if (agencia.equals(contas[verificaAgencias].getAgencia()) &&
+                            numero == contas[verificaAgencias].getNumero()) {
+                        menu2 = 1;
+                    } else {
                         System.out.println("Conta nao encontrada no sitema");
                     }
+                    while (menu2 == 1) {
+                        int z = 0;
+                        System.out.println("Ola " + contas[verificaAgencias].getNome() + " ");
+                        System.out.println("===============================================================");
+                        System.out.println("QUE OPERACAO DESEJA REALIZAR?");
+                        System.out.println("1. Fazer um deposito");
+                        System.out.println("2. Verificar situacao da conta");
+                        System.out.println("3. Fazer um saque");
+                        System.out.println("4. Sair");
+                        System.out.println("===============================================================");
+                        z = sc.nextInt();
 
+                        if (z == 1) {
+                            sc.nextLine();
+                            System.out.println("Digite o valor do deposito a ser efetuado");
+                            double deposito = sc.nextDouble();
+                            contas[verificaAgencias].fazDeposito(deposito);
+                        }
+
+                        if (z == 2) {
+                            System.out.println("O saldo do cliente " + contas[verificaAgencias].getNome() + " é de: "
+                                    + contas[verificaAgencias].getSaldo());
+                        }
+
+                        if (z == 3) {
+                            sc.nextLine();
+                            System.out.println("Digite o valor do saque a ser efetuado");
+                            double saque = sc.nextDouble();
+                            contas[verificaAgencias].fazSaque(saque);
+                        }
+
+                        if (z == 4) {
+                            menu2 = 0;
+                        }
+
+                    }
+                    break;
                 }
             }
-            if(i==3){
+            if (i == 3) {
+                sc.close();
                 break;
             }
-            
         }
-        sc.close();
     }
 }
-
